751928c77563c0512af77cb69c7c00fe8e72dff3
mihalichpalich <pavlovtfspb@gmail.com>
2020-07-29 10:58:57 +0300

    some changes for heroku

--- server/app.js
+++ server/app.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
+    introspection: true,
+    playground: true
 }));
 
 mongoose.connect(
